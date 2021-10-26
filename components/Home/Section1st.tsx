749eb9f5fe2f7bc6a0a2361b1c2ecdc1b3d604fe
William Lumbantobing <william.sl.tobing@gmail.com>
2021-10-26 13:31:14 +0700

    redirection to github pages

--- components/Home/Section1st.tsx
+++ components/Home/Section1st.tsx
@@ -1,10 +1,10 @@
 import { BeakerIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import ThemeHandler from "../ThemeHandler";
-import { CircuitSVG } from "./data";
+import { useCircuitSVG } from "./data";
 
 const Section1st = () => {
-  const [bgSVG, setBgSVG] = useState(CircuitSVG);
+  const [bgSVG] = useState(useCircuitSVG("ef4444"));
 
   return (
     <section className="relative h-full bg-gray-200 dark:bg-gray-800">
@@ -17,7 +17,7 @@ const Section1st = () => {
           <p className="text-6xl font-bold tracking-widest text-gray-900 dark:text-gray-100">
             William Suarez
           </p>
-          <p className="text-lg text-blue-600 dark:text-blue-400 font-bold italic tracking-wide sm:ml-1 mb-2">
+          <p className="text-lg text-red-500 font-bold italic tracking-wide sm:ml-1 mb-2">
             Lumbantobing
           </p>
           <div className="flex sm:block justify-center items-center mb-2">
@@ -32,15 +32,33 @@ const Section1st = () => {
           <p className="bg-gray-900 text-gray-100 sm:text-right uppercase sm:rounded-md px-4 py-2 my-2 font-bold sm:ml-1">
             #111827
           </p>
+          <div className="flex justify-center mt-4 animate-pulse">
+            <BeakerIcon className="h-6 w-6 mx-2 text-gray-900 dark:text-gray-100" />
+            <p className="text-gray-900 dark:text-gray-100 tracking-widest dark">
+              UNDER DEVELOPMENT
+            </p>
+            <BeakerIcon className="h-6 w-6 mx-2 text-gray-900 dark:text-gray-100" />
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-gray-900 dark:text-gray-100 tracking-wider dark">
+              Please refer to the following site.
+            </p>
+            <a
+              className="tracking-wider text-red-500 hover:text-red-600 hover:underline font-bold"
+              href="https://williamtobing.github.io/"
+            >
+              williamtobing.github.io
+            </a>
+          </div>
         </div>
       </div>
-      <div className="absolute bottom-4 w-full flex justify-center items-center animate-pulse">
+      {/* <div className="absolute bottom-4 w-full flex justify-center items-center animate-pulse">
         <BeakerIcon className="h-6 w-6 mx-2 text-gray-900 dark:text-gray-100" />
         <p className="text-gray-900 dark:text-gray-100 tracking-widest dark">
           UNDER DEVELOPMENT
         </p>
         <BeakerIcon className="h-6 w-6 mx-2 text-gray-900 dark:text-gray-100" />
-      </div>
+      </div> */}
     </section>
   );
 };
