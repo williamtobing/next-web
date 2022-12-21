271679c919830a9d13effb612e5b7c66dff5fe29
william <william.suarez@pintap.id>
2022-12-21 23:22:51 +0700

    feat(old): rollback, change npm to yarn, n update overlay message

--- components/InProgress.tsx
+++ components/InProgress.tsx
@@ -3,11 +3,17 @@ import { BeakerIcon } from "@heroicons/react/outline";
 const InProgress = () => {
   return (
     <div className="fixed bottom-2 w-full flex flex-col z-50">
-      <div className="flex justify-center md:justify-end animate-pulse">
+      <div className="flex justify-center md:justify-end">
         <BeakerIcon className="h-6 w-6 mx-2 d-text-reverse" />
         <p className="d-text-reverse text-sm md:text-base font-bold tracking-widest">
           {/* UNDER DEVELOPMENT */}
-          Too lazy to continue :)
+          Now latest:{" "}
+          <a
+            href="https://williamtobing.com"
+            className="text-red-600 hover:text-red-700 hover:underline font-black mt-2.5"
+          >
+            williamtobing.com
+          </a>
         </p>
         <BeakerIcon className="h-6 w-6 mx-2 d-text-reverse" />
       </div>
