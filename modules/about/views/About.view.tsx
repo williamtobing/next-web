2f3998fbe5df71b39f8903b3618f98cba343a469
william <william.suarez@pintap.id>
2022-12-21 19:11:58 +0700

    feat(FE-001): padding adj in about page

--- modules/about/views/About.view.tsx
+++ modules/about/views/About.view.tsx
@@ -5,13 +5,13 @@ import {Layout, Logo} from 'components/atoms';
 const About: React.FC = () => {
   return (
     <React.Fragment>
-      <Layout.Section css={tw`flex justify-center items-center pt-10 py-4`}>
+      <Layout.Section css={tw`flex justify-center items-center pt-10 pb-8`}>
         <div css={tw`bg-daisy-bush inline-block p-1 rounded-full`}>
           <Logo.TheLogoOne />
         </div>
       </Layout.Section>
 
-      <Layout.Section css={tw`flex justify-center items-center pb-8`}>
+      <Layout.Section css={tw`flex justify-center items-center pb-10`}>
         <div css={tw`inline-block p-1`}>
           <Logo.TheLogoTwo />
         </div>
